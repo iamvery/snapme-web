ba73b19a5240e7ef10233b620007bdf88d16d206
Jay Hayes <ur@iamvery.com>
2013-07-14 09:42:45 -0500

    Install rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'turnip/capybara'
+
+Dir[Rails.root.join("spec/(support|steps)/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order                                      = 'random'
+  config.use_transactional_fixtures                 = false
+end
