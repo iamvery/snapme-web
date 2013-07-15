c558e0a7304002db9e1bb39301ff1d1fcfffb92f
Jay Hayes <ur@iamvery.com>
2013-07-15 18:31:30 -0500

    Add snapshot uploader to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  mount_uploader :snapshot, SnapshotUploader
 end
---
+++ app/uploaders/snapshot_uploader.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+class SnapshotUploader < CarrierWave::Uploader::Base
+  storage :file
+end
