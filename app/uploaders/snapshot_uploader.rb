cd2c460685ffd89abe539a323c9d368303c8c3ac
Jay Hayes <ur@iamvery.com>
2013-07-16 21:42:40 -0500

    Set snapshot filename to numeric timestamp

--- app/uploaders/snapshot_uploader.rb
+++ app/uploaders/snapshot_uploader.rb
@@ -6,4 +6,8 @@ class SnapshotUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def filename
+    "#{Time.now.to_s(:number)}.#{file.extension}"
+  end
 end
