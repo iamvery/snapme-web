592f388c701802a0224b8c02ace4dcffa14995b3
Jay Hayes <ur@iamvery.com>
2013-07-18 17:11:17 -0500

    Add before_save filter to User model to ensure auth_token is set
    * Create an auth token as a secure randomized hex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save :ensure_auth_token
+
   mount_uploader :snapshot, SnapshotUploader
 
   def self.find_or_create_by_auth_hash(auth_hash)
@@ -7,5 +9,11 @@ class User < ActiveRecord::Base
     username = auth_hash.info.nickname
 
     find_or_create_by(provider: provider, uid: uid, username: username)
+  end
+
+  private
+
+  def ensure_auth_token
+    self.auth_token = SecureRandom.hex unless auth_token
   end
 end
