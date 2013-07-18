620b8390276772d502ed7949ccf9381a650079f6
Jay Hayes <ur@iamvery.com>
2013-07-17 23:14:05 -0500

    Convert snapshots to grayscale

--- app/uploaders/snapshot_uploader.rb
+++ app/uploaders/snapshot_uploader.rb
@@ -6,6 +6,7 @@ class SnapshotUploader < CarrierWave::Uploader::Base
   MAX_WIDTH = MAX_HEIGHT = 300
 
   storage :file
+  process :convert_to_grayscale
   process resize_to_fit: [MAX_WIDTH, MAX_HEIGHT]
 
   def store_dir
@@ -14,5 +15,15 @@ class SnapshotUploader < CarrierWave::Uploader::Base
 
   def filename
     "#{Time.now.to_s(:number)}.#{file.extension}"
+  end
+
+  private
+
+  def convert_to_grayscale
+    manipulate! do |image|
+      image.colorspace('Gray')
+      image = yield(image) if block_given?
+      image
+    end
   end
 end
