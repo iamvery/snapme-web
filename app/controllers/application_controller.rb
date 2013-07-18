b78451b158c4ce7f836052b6d7680468bb5e91ab
Jay Hayes <ur@iamvery.com>
2013-07-17 23:52:45 -0500

    Implement #authenticate controller filter
    * Add this before_filter to actions requiring authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def authenticate
+    redirect_to sign_in_path unless current_user
+  end
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
