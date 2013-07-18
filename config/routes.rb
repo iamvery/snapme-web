a1b72c3ed3ca3f9acbfc7be114e2c2555ec32a48
Jay Hayes <ur@iamvery.com>
2013-07-17 23:43:50 -0500

    Add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 APP_NAME::Application.routes.draw do
+  get    '/session/new' => 'sessions#new',     as: :sign_in
+  delete '/session'     => 'sessions#destroy', as: :sign_out
+  match  '/auth/:provider/callback' => 'sessions#create',  via: :all
+  match  '/auth/failure'            => 'sessions#failure', via: :all
+
   resources :users, only: %i(index) do
     resource :snapshot, only: %i(show create)
   end
