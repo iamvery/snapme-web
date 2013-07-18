49b474f409cf4d33a6f03ab906c92b7a584648cc
Jay Hayes <ur@iamvery.com>
2013-07-17 23:46:48 -0500

    Add User.find_or_create_by_auth_hash
    * Get or create a user by the omniauth authentication hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   mount_uploader :snapshot, SnapshotUploader
+
+  def self.find_or_create_by_auth_hash(auth_hash)
+    provider = auth_hash.provider
+    uid      = auth_hash.uid
+    username = auth_hash.info.nickname
+
+    find_or_create_by_provider_and_uid_and_username(provider, uid, username)
+  end
 end
