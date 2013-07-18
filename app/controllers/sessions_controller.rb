79a69f3b668a35c8b1cac629fdc8de4d44294cb9
Jay Hayes <ur@iamvery.com>
2013-07-18 00:55:26 -0500

    Display notices for successful sign ins and sign outs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_auth_hash(auth_hash)
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to root_path, notice: t('sign_in.success')
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to root_path
+    redirect_to root_path, notice: t('sign_out.success')
   end
 
   def failure
