a2b908fa56af38beee2756db254783c5a4b818a2
Jay Hayes <ur@iamvery.com>
2013-07-17 23:13:46 -0500

    Resize snapshots to fit within 300x300

--- app/uploaders/snapshot_uploader.rb
+++ app/uploaders/snapshot_uploader.rb
@@ -1,7 +1,12 @@
 # encoding: utf-8
 
 class SnapshotUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  MAX_WIDTH = MAX_HEIGHT = 300
+
   storage :file
+  process resize_to_fit: [MAX_WIDTH, MAX_HEIGHT]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
