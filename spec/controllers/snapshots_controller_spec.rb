c1e9dc505c47becf280aba7ed19b58286e870315
Jay Hayes <ur@iamvery.com>
2013-07-18 17:18:50 -0500

    Add specs for SnapshotsController
    * Don't worry about show action because it's going to go away once we
      hook up the super cool pusher events :)

---
+++ spec/controllers/snapshots_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe SnapshotsController do
+  describe 'POST #create' do
+    let(:user)    { double 'User'  }
+    let(:snapshot){ 'snapshot.jpg' }
+
+    before do
+      allow(User).to receive(:find).and_return(user)
+      allow(User).to receive(:find_by_auth_token).and_return(user)
+    end
+
+    it %q(updates the user's snapshot) do
+      expect(user).to receive(:snapshot=).with snapshot
+      expect(user).to receive(:save!)
+
+      expect(response).to be_success
+
+      post :create, user_id: 1, snapshot: snapshot
+    end
+  end
+end
