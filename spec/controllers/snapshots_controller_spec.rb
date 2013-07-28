06d38348150ba8a1273d453be5acaeeeb007615b
Jay Hayes <ur@iamvery.com>
2013-07-27 23:17:05 -0500

    Wrap snapshots controller spec in context

--- spec/controllers/snapshots_controller_spec.rb
+++ spec/controllers/snapshots_controller_spec.rb
@@ -5,19 +5,21 @@ describe SnapshotsController do
     let(:user)    { double 'User'  }
     let(:snapshot){ 'snapshot.jpg' }
 
-    before do
-      allow(User).to receive(:find).and_return(user)
-      allow(User).to receive(:find_by_auth_token).and_return(user)
-    end
+    context 'with authentication' do
+      before do
+        allow(User).to receive(:find).and_return(user)
+        allow(User).to receive(:find_by_auth_token).and_return(user)
+      end
 
-    it %q(updates the user's snapshot) do
-      expect(user).to receive(:snapshot=).with snapshot
-      expect(user).to receive(:save!)
-      expect(user).to receive(:notify_new_snapshot)
+      it %q(updates the user's snapshot) do
+        expect(user).to receive(:snapshot=).with snapshot
+        expect(user).to receive(:save!)
+        expect(user).to receive(:notify_new_snapshot)
 
-      expect(response).to be_success
+        expect(response).to be_success
 
-      post :create, user_id: 1, snapshot: snapshot
+        post :create, user_id: 1, snapshot: snapshot
+      end
     end
   end
 end
