3b79418465ddec41e765e9a2235173db5f4722c9
Jay Hayes <ur@iamvery.com>
2013-07-29 09:54:50 -0500

    Configure rspec to use database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'turnip/capybara'
+require 'database_cleaner'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 Dir[Rails.root.join("spec/steps/**/*.rb"  )].each { |f| require f }
@@ -14,7 +15,22 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order                                      = 'random'
-  config.use_transactional_fixtures                 = true
+  config.use_transactional_fixtures                 = false
+
+  default_db_cleaner_strategy = :transaction
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = default_db_cleaner_strategy
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 CarrierWave.configure do |config|
