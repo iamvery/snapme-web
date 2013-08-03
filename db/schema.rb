7c826049270985e93b6cf9c72e640ed4f9cb09cb
Jay Hayes <ur@iamvery.com>
2013-08-03 16:48:41 -0500

    Create invitations table in db
    * required string token
    
    [#14]

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130801231858) do
+ActiveRecord::Schema.define(version: 20130803213643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invitations", force: true do |t|
+    t.string "token", null: false
+  end
+
+  add_index "invitations", ["token"], name: "index_invitations_on_token", unique: true, using: :btree
 
   create_table "memberships", force: true do |t|
     t.integer "user_id", null: false
