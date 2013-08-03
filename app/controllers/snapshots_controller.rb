846813ac8ce747a83db10d9a719147864f7d451a
Jay Hayes <ur@iamvery.com>
2013-08-02 22:11:11 -0500

    Update SnapshotsController to accept snapshots as datauri or file
    * We're going to be offering snapshots directly from the browser using
      some wiz-bang HTML5. These files come in the form of data URIs so we
      need to be able to accept them for snapshots and Do the Right Thing.
    
    [#10]

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 class SnapshotsController < AuthenticatedController
   protect_from_forgery except: :create
 
@@ -6,11 +8,29 @@ class SnapshotsController < AuthenticatedController
     current_user.save!
     current_user.notify_new_snapshot
     head :ok
+  ensure
+    snapshot.close
+    snapshot.unlink if snapshot.is_a?(Tempfile)
   end
 
   private
 
-  def snapshot
+  def snapshot_param
     params[:snapshot]
+  end
+
+  def snapshot
+    @snapshot ||= if snapshot_param.is_a?(String)
+      encoded_image_data = snapshot_param.split(',').last
+      image_data         = Base64.decode64(encoded_image_data)
+
+      file = Tempfile.open('data-uri-file') do |f|
+        f.binmode
+        f.write(image_data)
+        f
+      end
+    else
+      snapshot_param
+    end
   end
 end
