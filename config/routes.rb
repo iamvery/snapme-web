663639e5241c75b007af02837cc71d53e6d5c117
Jay Hayes <ur@iamvery.com>
2013-08-04 02:36:18 -0500

    Add invitation request routes and translations
    [#14]

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ APP_NAME::Application.routes.draw do
   match  '/auth/:provider/callback' => 'sessions#create',  via: :all
   match  '/auth/failure'            => 'sessions#failure', via: :all
 
+  resources :invitation_requests, except: %i(show edit update) do
+    post '/send' => 'invitation_requests#send_invitation', as: :send
+  end
   resources :memberships, only: %i(create destroy)
   resource  :snapshot, only: %i(create)
   resources :teams
