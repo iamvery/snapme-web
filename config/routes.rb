4d53bf35f5297c74dc63106f8a116c2b31b42f35
Jay Hayes <ur@iamvery.com>
2013-08-04 02:35:46 -0500

    Clean up some translations and organize routes
    [#chore]

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ APP_NAME::Application.routes.draw do
   match  '/auth/:provider/callback' => 'sessions#create',  via: :all
   match  '/auth/failure'            => 'sessions#failure', via: :all
 
-  resources :teams
   resources :memberships, only: %i(create destroy)
   resource  :snapshot, only: %i(create)
+  resources :teams
 
   root to: 'teams#index'
 end
